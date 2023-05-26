5ab30ec4665b7fb92e3be8d3d1482f5c23a0e3c0
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-26 20:21:00 +0800

    Getter And Setter

--- src/main/java/com/mycompany/praktek03/SepedaMotor.java
+++ src/main/java/com/mycompany/praktek03/SepedaMotor.java
@@ -5,5 +5,14 @@ public class SepedaMotor extends Sepeda {
     void throttle(){
         System.out.println("wuuushhh");
     }
+
+    public String getJenisMotor() {
+        return jenisMotor;
+    }
+
+    public void setJenisMotor(String jenisMotor) {
+        this.jenisMotor = jenisMotor;
+    }
+    
     
 }
