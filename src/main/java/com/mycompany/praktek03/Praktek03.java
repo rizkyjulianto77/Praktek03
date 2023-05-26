ad5db50d08d33e157c52a4263097384729d95c45
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-26 19:34:47 +0800

    Membuat Atribut

---
+++ src/main/java/com/mycompany/praktek03/Praktek03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktek03;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class Praktek03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/praktek03/Sepeda.java
@@ -0,0 +1,7 @@
+package com.mycompany.praktek03;
+public class Sepeda {
+    String merk;
+    String warna;
+    double harga;
+    
+}
