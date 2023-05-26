c1d78dff4203b3026f92135f78425a1e7ee609c2
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-26 19:46:39 +0800

    Instance, buat atribut dan panggil method

--- src/main/java/com/mycompany/praktek03/SepedaAksi.java
+++ src/main/java/com/mycompany/praktek03/SepedaAksi.java
@@ -1,7 +1,13 @@
 package com.mycompany.praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
+        Sepeda s = new Sepeda();
         
+        s.merk=("Kupu-kupu");
+        s.warna=("Merah");
+        s.harga=(1000);
+        
+        s.cetakInfo();
     }
     
 }
