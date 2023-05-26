d17640c61dbce7d855dc12df9b79e8ea137f584f
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-26 19:53:56 +0800

    Menggunakan Method Set

--- src/main/java/com/mycompany/praktek03/SepedaAksi.java
+++ src/main/java/com/mycompany/praktek03/SepedaAksi.java
@@ -3,9 +3,9 @@ public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
         
-        s.merk=("Kupu-kupu");
-        s.warna=("Merah");
-        s.harga=(1000);
+        s.setMerk("Butterfly");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }
